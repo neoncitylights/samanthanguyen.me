7cf50d88cd1b407479270dbbbac5eef26f2b63bf
Samantha Nguyen <contact@samanthanguyen.me>
2024-12-18 15:21:09 -0600

    semantics + favicons

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,17 +2,19 @@
 	import { twJoin } from 'tailwind-merge'
 </script>
 
-<header class="flex flex-col gap-1 lg:gap-2">
-	<h1 class={twJoin(
-		"pt-12 flex items-end",
-	 	"text-2xl lg:text-4xl font-domine font-bold lg:font-medium",
-	)}>
-		samanthanguyen.me
-	</h1>
-	<span class={twJoin(
-		"text-sm lg:text-base dark:text-zinc-500",
-		"font-domine",
-	)}>
-		she / her. artist, software engineer, aspiring computer scientist
-	</span>
+<header>
+	<hgroup class="flex flex-col gap-1 lg:gap-2">
+		<h1 class={twJoin(
+			"pt-12 flex items-end",
+		 	"text-2xl lg:text-4xl font-domine font-bold lg:font-medium",
+		)}>
+			samanthanguyen.me
+		</h1>
+		<p class={twJoin(
+			"text-sm lg:text-base dark:text-zinc-500",
+			"font-domine",
+		)}>
+			she / her. artist, software engineer, aspiring computer scientist
+		</p>
+	</hgroup>
 </header>
