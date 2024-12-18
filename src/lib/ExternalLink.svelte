8901269a423df9099450091047ea78a494b112b0
Samantha Nguyen <contact@samanthanguyen.me>
2024-12-17 21:05:25 -0600

    external links for footer

---
+++ src/lib/ExternalLink.svelte
@@ -0,0 +1,7 @@
+<script>
+	let { children, ...props } = $props()
+</script>
+
+<a {...props} target="_blank" rel="noopener noreferrer">
+	{@render children()}
+</a>
--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { IconBrandBluesky, IconBrandGithubFilled, IconMailFilled } from '@tabler/icons-svelte'
 	import { twJoin } from 'tailwind-merge'
+    import ExternalLink from './ExternalLink.svelte'
 </script>
 
 <div class="h-0 lg:h-[1px] bg-separator-light dark:bg-separator-dark"></div>
@@ -8,23 +9,29 @@
 	"mt-[-1rem] lg:mt-0 flex flex-row justify-end",
 )}>
 	<span class="flex flex-row gap-4">
-		<a href="mailto:contact@samanthanguyen.me" title="Email me at contact@samanthanguyen.me">
+		<ExternalLink
+			href="mailto:contact@samanthanguyen.me"
+			title="Email me at contact@samanthanguyen.me">
 			<IconMailFilled
 				size={32}
 				stroke={1}
 				class={twJoin("fill-[#CFB4C5] dark:fill-neutral-300")} />
-		</a>
-		<a href="https://bsky.app/profile/samanthanguyen.me" title="@samanthanguyen.me on Bluesky">
+		</ExternalLink>
+		<ExternalLink
+			href="https://bsky.app/profile/samanthanguyen.me"
+			title="@samanthanguyen.me on Bluesky">
 			<IconBrandBluesky
 				size={32}
 				stroke={1}
 				class={twJoin("text-[#CFB4C5] fill-[#CFB4C5]", "dark:text-neutral-300 dark:fill-neutral-300")} />
-		</a>
-		<a href="https://github.com/neoncitylights" title="@neoncitylights on GitHub">
+		</ExternalLink>
+		<ExternalLink
+			href="https://github.com/neoncitylights"
+			title="@neoncitylights on GitHub">
 			<IconBrandGithubFilled
 				size={32}
 				stroke={1}
 				class={twJoin("fill-[#CFB4C5]", "dark:fill-neutral-300")} />
-		</a>
+		</ExternalLink>
 	</span>
 </footer>
