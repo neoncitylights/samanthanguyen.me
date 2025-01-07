ef5fa6eaed3e2d4d7ef23f3e188aaff9a66f4c55
Samantha <contact@samanthanguyen.me>
2025-01-07 00:54:20 -0600

    Footer tooltips (#3)

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import ExternalLink from '$lib/ExternalLink.svelte'
-	import { IconArrowUpRight } from '@tabler/icons-svelte'
 
 	const socials = [
 		{
@@ -28,21 +27,63 @@
 ]}>
 	<span class={[
 		'flex flex-row gap-4 lg:gap-6 justify-end',
-		'',
 	]}>
 		{#each socials as link}
-			<ExternalLink
-				href={link.href}
-				title={link.title}
-				class={[
-					'flex flex-row items-center gap-0.5 lg:gap-1',
-					'text-xs lg:text-sm font-mono',
-					'hover:bg-zinc-900 hover:text-zinc-100',
-					'dark:hover:bg-zinc-100 dark:hover:text-zinc-900',
-				]}
-			>
-				{link.text}<IconArrowUpRight size={20} />
-			</ExternalLink>
+			<span>
+				<ExternalLink
+					href={link.href}
+					id={`socials-anchor-${link.text.toLowerCase()}`}
+					class={[
+						'socials-anchor',
+						'flex flex-row items-center gap-1 lg:gap-2',
+						'text-xs lg:text-sm font-mono',
+						'hover:bg-zinc-900 hover:text-zinc-100',
+						'dark:hover:bg-zinc-100 dark:hover:text-zinc-900'
+						// 'hover:bg-blue-600 hover:text-zinc-100',
+						// 'dark:hover:bg-zinc-100 dark:hover:text-zinc-900',
+					]}
+				>
+					{`${link.text} \u{2197}`}
+				</ExternalLink>
+				<span
+					class={[
+						'socials-target hidden absolute p-2 mt-1',
+						'bg-white border-2 border-black',
+						'dark:bg-zinc-900 dark:border-zinc-100',
+						'text-xs',
+						'text-zinc-900 dark:text-zinc-100',
+						'transition-all'
+					]}
+					id={`socials-target-${link.text.toLowerCase()}`}
+				>
+					{`\u{00ab} ${link.title} \u{00bb}`}
+				</span>
+			</span>
 		{/each}
 	</span>
 </footer>
+
+<style>
+	.socials-target {
+		position-area: span-bottom start;
+		position-visibility: no-overflow;
+		box-shadow: 4px 4px #000;
+	}
+	@media (prefers-color-scheme: dark) {
+		.socials-target {
+			box-shadow: 4px 4px #fff;
+		}
+	}
+
+	:global(#socials-anchor-email)   { anchor-name:       --email-anchor; }
+	:global(#socials-target-email)   { position-anchor:   --email-anchor; }
+	:global(#socials-anchor-bluesky) { anchor-name:     --bluesky-anchor; }
+	:global(#socials-target-bluesky) { position-anchor: --bluesky-anchor; }
+	:global(#socials-anchor-github)  { anchor-name:      --github-anchor; }
+	:global(#socials-target-github)  { position-anchor:  --github-anchor; }
+	@media (hover: hover) {
+		:global(.socials-anchor:hover ~ .socials-target) {
+			display: block;
+		}
+	}
+</style>
